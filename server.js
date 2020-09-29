746d6c8c3d43e074321ef7012058e82570a46c95
Andrei Ishchenko <71075472+c1nnam0nbun@users.noreply.github.com>
2020-09-29 10:37:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const getWindDirection = require('./helpers/getWindDirection')
 
 require('dotenv').config()
 const app = express()
-app.use(express.static('public'))
+app.use('/', express.static(__dirname + '/public/index.html'))
 
 const today = new Date()
 const db = mongoose.connection
