36a94ac6da2efcb600e2230d61f24f75cf7c2117
Andrei Ishchenko <71075472+c1nnam0nbun@users.noreply.github.com>
2020-09-29 15:07:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ db.on('connected', () => {
 
     app.listen(process.env.PORT || 8080, () => console.log('Listening...'))
 
-    setInterval(() => destinations.forEach(city => getWeather(city)), 1000*60*60)
+    setInterval(() => destinations.forEach(city => getWeather(city)), 3600000)
 
     let canRun = true
     setInterval(() => {
